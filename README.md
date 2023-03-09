9c767ccb1d08e40dffa40f208848a215f1caf4a8
Norman Monteiro <montein56@hotmail.com>
2023-03-08 20:46:44 -0500

    weather app site almost ready

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## WEATHER APP
 
-This app creates a weather forecast site using the weather API from Open Weather. You should be able to search for a specific location and toggle displaying the data in Fahrenheit or Celsius.
+This app creates a weather forecast site using the weather API from Open Weather.
+You should be able to search for a specific location and toggle displaying the data in Fahrenheit or Celsius.
+
+# Additional Feature
+
+Displays gifs from giphy.com related to the state of the weather
--- index.js
+++ index.js
@@ -1,20 +1,84 @@
-async function refreshing(newGifItem) {
-  if (newGifItem === undefined) {
-    newGifItem = "rock";
-  }
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=2n7POPaMC5FWod3I9Ag5QQmYZ96U6CKP&s=${newGifItem}`,
+function displayResults(newCity, weatherData, giffyUrl, unit) {
+  const display = document.getElementById("display");
+  const displayCity = document.getElementById("displayCity");
+  const main = weatherData.main;
+  const weather = weatherData.weather;
+  let tempUnit = unit === "metric" ? "C" : "F";
+  // if (unit === "metric") {
+  //   tempUnit = "C";
+  // } else {
+  //   tempUnit = "F";
+  // }
+  document.getElementById(
+    "icon"
+  ).src = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+  displayCity.innerText =
+    `${newCity}`.toUpperCase() + `, ${weatherData.sys.country}`;
+
+  display.innerText =
+    `\n${weather[0].main} : ${weather[0].description}` +
+    `\n\n${Math.round(main.temp, 2)}\u00B0 ${tempUnit}` +
+    `,  Feels like ${Math.round(main.feels_like, 2)}\u00B0 ${tempUnit}` +
+    `\n\nMax ${Math.round(main.temp_max, 2)}\u00B0 ${tempUnit}` +
+    ` & Min ${Math.round(main.temp_min, 2)}\u00B0 ${tempUnit}` +
+    `\n\nHumidity ${main.humidity} %` +
+    `\n\nPressure ${main.pressure}`;
+  const image = document.getElementById("gifImage");
+  image.src = giffyUrl.data.images.original.url;
+}
+
+function getInfo(newLocation, unit) {
+  getWeather(newLocation, unit)
+    .then((results) => {
+      console.log(results.openWeather);
+      console.log(results.giffy);
+      const weatherData = results.openWeather;
+      const giffyUrl = results.giffy;
+      const newCity = results.city;
+      displayResults(newCity, weatherData, giffyUrl, unit);
+    })
+    .catch((err) => console.log(err));
+}
+
+function changeUnits() {
+  const location = document.getElementById("displayCity").innerText;
+  const newLocation = location.substring(0, location.length - 4);
+  const unit = this.checked ? "metric" : "imperial";
+  getInfo(newLocation, unit);
+}
+
+function searchLocation() {
+  const place = document.getElementById("location");
+  const newLocation = place.value;
+  const x = document.getElementById("myCheck").checked;
+  const unit = x ? "metric" : "imperial";
+  getInfo(newLocation, unit);
+  place.value = "";
+}
+
+async function getWeather(newLocation, unit) {
+  const response1 = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${newLocation}&appid=7cdfc03226332fb7b28028a76302e23a&units=${unit}`,
     { mode: "cors" }
   );
-  response
-    .json()
-    .then(function (response) {
-      image.src = response.data.images.original.url;
-    })
-    .catch(function (err) {
-      const span = document.getElementById("errorSpan");
-      span.textContent = "You searched for WHAT??? :)";
-      refreshing("laugh");
-    });
+  const json1 = await response1.json();
+  const response2 = await fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=2n7POPaMC5FWod3I9Ag5QQmYZ96U6CKP&s=${json1.weather[0].description}`,
+    { mode: "cors" }
+  );
+  const json2 = await response2.json();
+  return {
+    openWeather: json1,
+    giffy: json2,
+    city: newLocation,
+  };
 }
-refreshing();
+
+function addListeners() {
+  const checkbox = document.getElementById("myCheck");
+  checkbox.addEventListener("change", changeUnits);
+  const btnSearch = document.getElementById("btnSearch");
+  btnSearch.addEventListener("click", searchLocation);
+}
+addListeners();
